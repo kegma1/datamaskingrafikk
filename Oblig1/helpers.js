7ec877f5368024ffcb43ae98281b170d9dadfbf4
Kennet Mathisen <kennetam02@gmail.com>
2024-09-03 14:08:25 +0200

    changed to gl-matrix

--- Oblig1/helpers.js
+++ Oblig1/helpers.js
@@ -1,18 +1,17 @@
 export class Scene {
-    constructor(gl, camara, objects = {}) {
+    constructor(gl, camera, objects = {}) {
         this.gl = gl;
-        this.camera = camara;
-        this.objects = objects
+        this.camera = camera;
+        this.objects = objects;
         
         /**@type {function(Object): void} [setup] - Callback function that takes Objects as parameters. */
         this.setup = () => {};
-        /**@type {function(Object,number, number): void} [update] - Callback function that takes Objects, timeElapsed and deltaTime as parameters. */
+        /**@type {function(Object, number, number): void} [update] - Callback function that takes Objects, timeElapsed, and deltaTime as parameters. */
         this.update = () => {};
     }
 
     draw() {
         clearCanvas(this.gl);
-        
 
         for (const k in this.objects) {
             this.objects[k].bind(this.camera);
@@ -27,18 +26,16 @@ export class Scene {
             const deltaTime = timestamp - (this.lastFrameTime || timestamp);
             this.lastFrameTime = timestamp;
 
-
             this.update(this.objects, timeElapsed / 1000, deltaTime / 1000);
 
             this.draw();
-            requestAnimationFrame(renderLoop)
-        }
+            requestAnimationFrame(renderLoop);
+        };
 
-        this.setup(this.objects)
-        requestAnimationFrame(renderLoop)
+        this.setup(this.objects);
+        requestAnimationFrame(renderLoop);
     }
 }
-
 
 export class MeshInstance {
     /**
@@ -50,31 +47,29 @@ export class MeshInstance {
     constructor(mesh, shaderInstance, shaderParams) {
         this.mesh = mesh;
         this.shader = shaderInstance;
-        this.shaderParams = shaderParams
+        this.shaderParams = shaderParams;
 
-        this.position = new Vector3([0, 0, 0])
-        this.rotation = new Matrix4()
-        this.scale = new Vector3([1, 1, 1])
+        this.position = vec3.create();
+        this.rotation = mat4.create();
+        this.scale = vec3.fromValues(1, 1, 1);
     }
-
-    
 
     bind(camera) {
         const constants = this.shaderParams;
 
-        const transformation = new Matrix4();
-        transformation.translate(this.position.elements[0], this.position.elements[1], this.position.elements[2]);
-        transformation.multiply(this.rotation)
-        transformation.scale(this.scale.elements[0], this.scale.elements[1], this.scale.elements[2]);
+        const transformation = mat4.create();
+        mat4.multiply(transformation, this.rotation, transformation);
+        mat4.translate(transformation, transformation, this.position);
+        mat4.scale(transformation, transformation, this.scale);
 
-        let modelMatrix = new Matrix4();
-	    modelMatrix.setIdentity();
-        modelMatrix.multiply(transformation);
-        
+        let modelMatrix = mat4.create();
+        mat4.identity(modelMatrix);
+        mat4.multiply(modelMatrix, transformation, modelMatrix);
 
-	    let modelViewMatrix = new Matrix4(new Matrix4(camera.viewMatrix).multiply(modelMatrix));
+        let modelViewMatrix = mat4.create();
+        mat4.multiply(modelViewMatrix, camera.viewMatrix, modelMatrix);
 
-	    this.shader.set("projectionMatrix", camera.projectionMatrix);
+        this.shader.set("projectionMatrix", camera.projectionMatrix);
         this.shader.set("modelViewMatrix", modelViewMatrix);
 
         this.shader.bind(constants);
@@ -82,7 +77,7 @@ export class MeshInstance {
     }
 
     draw() {
-        this.mesh.draw()
+        this.mesh.draw();
     }
 }
 
@@ -98,10 +93,10 @@ export class ShaderInstance {
             this.uniforms[k] = {
                 ...shader.uniforms[k],
                 value: null,
-            }
+            };
         }
 
-        this.attributs = {...shader.attributs};
+        this.attributes = {...shader.attributes};
     }
 
     set(name, v) {
@@ -123,36 +118,30 @@ export class ShaderInstance {
             if (value && u.location) {
                 const t = u.type;
                 
-                if (t == "Matrix4") {
-                    gl.uniformMatrix4fv(u.location, false, value.elements);
-                } else if (t == "Matrix3") {
-                    gl.uniformMatrix3fv(u.location, false, value.elements);
-                } else if (t == "Vector4") {
-                    gl.uniform4fv(u.location, value.elements);
-                } else if (t == "Vector3") {
-                    gl.uniform3fv(u.location, value.elements);
-                } else if (t == "Vector2") {
-                    gl.uniform2fv(u.location, value.elements);
-                } 
-
+                if (t === "mat4") {
+                    gl.uniformMatrix4fv(u.location, false, value);
+                } else if (t === "vec4") {
+                    gl.uniform4fv(u.location, value);
+                } else if (t === "vec3") {
+                    gl.uniform3fv(u.location, value);
+                } else if (t === "vec2") {
+                    gl.uniform2fv(u.location, value);
+                }
             }
         }
-
     }
 }
 
 export class Mesh {
     constructor(gl, vertexData, vertexLength) {
         /**@type {WebGL2RenderingContext} */
-        this.gl = gl
-        this.buffers = {}
+        this.gl = gl;
+        this.buffers = {};
 
-
-        this.vertexData = vertexData
-        this.vertexLength = vertexLength
+        this.vertexData = vertexData;
+        this.vertexLength = vertexLength;
 
         this.bufferData({data: vertexData}, "vertexData");
-        // this.bufferData({data: colors}, "colors");
     }
 
     /**
@@ -168,7 +157,7 @@ export class Mesh {
         this.gl.bufferData(this.gl.ARRAY_BUFFER, new Float32Array(info.data), this.gl.STATIC_DRAW);
         this.gl.bindBuffer(this.gl.ARRAY_BUFFER, null);
 
-        this.buffers[name] = info
+        this.buffers[name] = info;
     }
 
     /**
@@ -176,27 +165,18 @@ export class Mesh {
      * @param {Shader} shader 
      */
     bind(shader) {
-        for (let k in shader.attributs) {
+        for (let k in shader.attributes) {
             connectAttribute(
-                this.gl, 
-                shader.attributs[k].location, 
-                this.buffers.vertexData.buffer, 
-                shader.attributs[k].size,
-                shader.attributs[k].type,
-                shader.attributs[k].normalize,
-                shader.attributs[k].stride,
-                shader.attributs[k].offset 
+                this.gl,
+                shader.attributes[k].location,
+                this.buffers.vertexData.buffer,
+                shader.attributes[k].size,
+                shader.attributes[k].type,
+                shader.attributes[k].normalize,
+                shader.attributes[k].stride,
+                shader.attributes[k].offset 
             );
         }
-        // connectAttribute(
-        //     this.gl, 
-        //     shader.attributs.vertexPosition.location, 
-        //     this.buffers.vertexData.buffer, 
-        //     shader.attributs.vertexPosition.size,
-            
-        //     shader.attributs.vertexPosition.stride,
-        //     shader.attributs.vertexPosition.offset);
-        // connectAttribute(this.gl, shader.attributs.vertexColor.location, this.buffers.vertexData.buffer, 4);
     }
 
     draw() {
@@ -205,34 +185,34 @@ export class Mesh {
 }
 
 export class Shader {
-    constructor(gl, vertexSource, fragmentSource, attribueParams, uniformParams) {
+    constructor(gl, vertexSource, fragmentSource, attributeParams, uniformParams) {
         /**@type {WebGL2RenderingContext} */
         this.gl = gl;
         this.vertexSource = vertexSource;
         this.fragmentSource = fragmentSource;
    
-		this.vertexShader = this.compileShader(gl, gl.VERTEX_SHADER, vertexSource);
-		this.fragmentShader = this.compileShader(gl, gl.FRAGMENT_SHADER, fragmentSource);
+        this.vertexShader = this.compileShader(gl, gl.VERTEX_SHADER, vertexSource);
+        this.fragmentShader = this.compileShader(gl, gl.FRAGMENT_SHADER, fragmentSource);
 
-		this.shaderProgram = gl.createProgram();
-		gl.attachShader(this.shaderProgram, this.vertexShader);
-		gl.attachShader(this.shaderProgram, this.fragmentShader);
-		gl.linkProgram(this.shaderProgram);
-		if (!gl.getProgramParameter(this.shaderProgram, gl.LINK_STATUS)) {
-			alert('Feil ved kompilering og/eller linking av shaderprogrammene: ' + gl.getProgramInfoLog(this.shaderProgram));
-		}
+        this.shaderProgram = gl.createProgram();
+        gl.attachShader(this.shaderProgram, this.vertexShader);
+        gl.attachShader(this.shaderProgram, this.fragmentShader);
+        gl.linkProgram(this.shaderProgram);
+        if (!gl.getProgramParameter(this.shaderProgram, gl.LINK_STATUS)) {
+            alert('Failed to compile and/or link shaders: ' + gl.getProgramInfoLog(this.shaderProgram));
+        }
 
-        this.attributs = {};
-		this.uniforms = {};
+        this.attributes = {};
+        this.uniforms = {};
 
-        for(let k in attribueParams) {
-            this.attributs[k] = {
-                location: this.gl.getAttribLocation(this.shaderProgram, attribueParams[k].name),
-                ...attribueParams[k],
+        for (let k in attributeParams) {
+            this.attributes[k] = {
+                location: this.gl.getAttribLocation(this.shaderProgram, attributeParams[k].name),
+                ...attributeParams[k],
             };
         }
 
-        for(let k in uniformParams) {
+        for (let k in uniformParams) {
             this.uniforms[k] = {
                 location: this.gl.getUniformLocation(this.shaderProgram, uniformParams[k].name),
                 type: uniformParams[k].type,
@@ -241,16 +221,16 @@ export class Shader {
     }
 
     compileShader(gl, type, source) {
-		const shader = gl.createShader(type);
-		gl.shaderSource(shader, source);
-		gl.compileShader(shader);
-		if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
-			alert('En feil oppsto ved kompilering av shaderne: ' + gl.getShaderInfoLog(shader));
-			gl.deleteShader(shader);
-			return null;
-		}
-		return shader;
-	}
+        const shader = gl.createShader(type);
+        gl.shaderSource(shader, source);
+        gl.compileShader(shader);
+        if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+            alert('An error occurred while compiling shaders: ' + gl.getShaderInfoLog(shader));
+            gl.deleteShader(shader);
+            return null;
+        }
+        return shader;
+    }
 
     bind() {
         this.gl.useProgram(this.shaderProgram);
@@ -260,29 +240,26 @@ export class Shader {
 export class Camera {
     /**
      * 
-     * @param {Vector3} pos 
-     * @param {Vector3} lookAt 
+     * @param {vec3} pos 
+     * @param {vec3} lookAt 
      * @param {Number} FOV 
      * @param {Number} aspectRatio 
      * @param {Number} near 
      * @param {Number} far 
      */
     constructor(pos, lookAt, FOV, aspectRatio, near, far) {
-        /**@type {Vector3} */
-        this.cam_pos = pos
+        /**@type {vec3} */
+        this.cam_pos = pos;
+        /**@type {vec3} */
+        this.look = lookAt;
+        /**@type {vec3} */
+        this.up = vec3.fromValues(0, 1, 0);
 
+        /**@type {mat4} */
+        this.viewMatrix = mat4.create();
+        /**@type {mat4} */
+        this.projectionMatrix = mat4.create();
 
-        /**@type {Vector3} */
-        this.look = lookAt
-        /**@type {Vector3} */
-        this.up = new Vector3([0, 1, 0]);
-
-        /**@type {Matrix4} */
-        this.viewMatrix = new Matrix4();
-        /**@type {Matrix4} */
-	    this.projectionMatrix = new Matrix4();
-
-        
         /**@type {Number} */
         this.FOV = FOV;
 
@@ -294,59 +271,61 @@ export class Camera {
 
         /**@type {Number} */
         this.far = far;
-        
 
-        this.update_view()
+        this.update_view();
     }
 
-    get pos() {return this.cam_pos}
+    get pos() { return this.cam_pos; }
     set pos(new_pos) {
-        this.cam_pos = new_pos
-        
-        this.update_view()
+        this.cam_pos = new_pos;
+        this.update_view();
     }
 
-    get lookAt() {return this.look}
+    get lookAt() { return this.look; }
     set lookAt(new_lookAt) {
-        this.look = new_lookAt
-        
-        this.update_view()
+        this.look = new_lookAt;
+        this.update_view();
     }
 
     update_view() {
-        this.viewMatrix.setLookAt(this.cam_pos.elements[0], this.cam_pos.elements[1], this.cam_pos.elements[2], this.look.elements[0], this.look.elements[1], this.look.elements[2], this.up.elements[0], this.up.elements[1], this.up.elements[2]);
-        this.projectionMatrix.setPerspective(this.FOV, this.aspectRatio, this.near, this.far);
+        mat4.lookAt(this.viewMatrix, this.cam_pos, this.look, this.up);
+        mat4.perspective(this.projectionMatrix, this.FOV, this.aspectRatio, this.near, this.far);
     }
-
 }
 
 /**
  * 
  * @param {WebGL2RenderingContext} gl 
- * @param {Number} attributLocation 
- * @param {Float32Array} positionBuffer 
+ * @param {Number} attributeLocation 
+ * @param {WebGLBuffer} buffer 
+ * @param {Number} numComponents 
+ * @param {Number} type 
+ * @param {Boolean} normalize 
+ * @param {Number} stride 
+ * @param {Number} offset 
  */
-export function connectAttribute(gl, attributLocation, Buffer, numComponents = 3, type = gl.FLOAT, normalize = false, stride = 0, offset = 0) {
-	gl.bindBuffer(gl.ARRAY_BUFFER, Buffer);
-	gl.vertexAttribPointer(
-		attributLocation,
-		numComponents,
-		type,
-		normalize,
-		stride,
-		offset);
-	gl.enableVertexAttribArray(attributLocation);
+export function connectAttribute(gl, attributeLocation, buffer, numComponents = 3, type = gl.FLOAT, normalize = false, stride = 0, offset = 0) {
+    gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
+    gl.vertexAttribPointer(
+        attributeLocation,
+        numComponents,
+        type,
+        normalize,
+        stride,
+        offset
+    );
+    gl.enableVertexAttribArray(attributeLocation);
 }
 
 /**
- * Klargjør canvaset.
- * Kalles fra draw()
+ * Clears the canvas.
+ * Called from draw()
  * @param {WebGL2RenderingContext} gl 
  */
 export function clearCanvas(gl) {
-	gl.clearColor(0.75, 0.75, 0.75 , 1);  // Clear screen farge.
-	gl.clearDepth(1.0);
-	gl.enable(gl.DEPTH_TEST);           // Enable "depth testing".
-	gl.depthFunc(gl.LEQUAL);            // Nære objekter dekker fjerne objekter.
-	gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+    gl.clearColor(0.75, 0.75, 0.75, 1);  // Clear color.
+    gl.clearDepth(1.0);
+    gl.enable(gl.DEPTH_TEST);           // Enable depth testing.
+    gl.depthFunc(gl.LEQUAL);            // Near objects obscure far objects.
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 }
--- Oblig1/main.js
+++ Oblig1/main.js
@@ -1,83 +1,81 @@
 import {WebGLCanvas} from '../webgl24_std/base/helpers/WebGLCanvas.js';
-import {Camera, Mesh, Shader, MeshInstance, ShaderInstance, Scene} from "./helpers.js"
+import {Camera, Mesh, Shader, MeshInstance, ShaderInstance, Scene} from "./helpers.js";
 
 
 export function main() {
-	// Oppretter et webGLCanvas for WebGL-tegning:
-	const webGLCanvas = new WebGLCanvas('myCanvas', document.body, 600, 600);
+    // Create a WebGLCanvas for WebGL drawing:
+    const webGLCanvas = new WebGLCanvas('myCanvas', document.body, 600, 600);
 
-	let vertexShaderSource = document.getElementById('base-vertex-shader').innerHTML;
-	let fragmentShaderSource = document.getElementById('base-fragment-shader').innerHTML;
+    let vertexShaderSource = document.getElementById('base-vertex-shader').innerHTML;
+    let fragmentShaderSource = document.getElementById('base-fragment-shader').innerHTML;
 
-	let gl = webGLCanvas.gl;
+    let gl = webGLCanvas.gl;
 
-	let pyramid = new Mesh(gl, [
-		0, 1, 0,	 0.25, 0.25, 0.25, 1, // X Y Z	R G B A
-		-1, 0, 1,	 0.25, 0.25, 0.25, 1, // X Y Z	R G B A
-		1, 0, 1,	 0.25, 0.25, 0.25, 1, // X Y Z	R G B A
+    let pyramid = new Mesh(gl, [
+        0, 1, 0,    0.25, 0.25, 0.25, 1, // X Y Z R G B A
+        -1, 0, 1,    0.25, 0.25, 0.25, 1, // X Y Z R G B A
+        1, 0, 1,    0.25, 0.25, 0.25, 1, // X Y Z R G B A
 
-		0, 1, 0,	 0.25, 0.25, 0.25, 1, // X Y Z	R G B A
-		1, 0, 1,	 -0.5, -0.5, -0.5, 1, // X Y Z	R G B A
-		1, 0 , -1,	 -0.5, -0.5, -0.5, 1, // X Y Z	R G B A
+        0, 1, 0,    0.25, 0.25, 0.25, 1, // X Y Z R G B A
+        1, 0, 1,    -0.5, -0.5, -0.5, 1, // X Y Z R G B A
+        1, 0, -1,    -0.5, -0.5, -0.5, 1, // X Y Z R G B A
 
-		0, 1, 0,	 0.25, 0.25, 0.25, 1, // X Y Z	R G B A
-		-1, 0, -1,	 -0.5, -0.5, -0.5, 1, // X Y Z	R G B A
-		-1, 0 , 1,	 -0.5, -0.5, -0.5, 1, // X Y Z	R G B A
+        0, 1, 0,    0.25, 0.25, 0.25, 1, // X Y Z R G B A
+        -1, 0, -1,    -0.5, -0.5, -0.5, 1, // X Y Z R G B A
+        -1, 0, 1,    -0.5, -0.5, -0.5, 1, // X Y Z R G B A
 
-		0, 1, 0,	 0.25, 0.25, 0.25, 1, // X Y Z	R G B A
-		-1, 0, -1,	 -0.5, -0.5, -0.5, 1, // X Y Z	R G B A
-		1, 0, -1,	 -0.5, -0.5, -0.5, 1, // X Y Z	R G B A
+        0, 1, 0,    0.25, 0.25, 0.25, 1, // X Y Z R G B A
+        -1, 0, -1,    -0.5, -0.5, -0.5, 1, // X Y Z R G B A
+        1, 0, -1,    -0.5, -0.5, -0.5, 1, // X Y Z R G B A
 
-		1, 0, 1,	 -0.5, -0.5, -0.5, 1, // X Y Z	R G B A
-		1, 0, -1,	 -0.5, -0.5, -0.5, 1, // X Y Z	R G B A
-		-1, 0, -1,	 -0.5, -0.5, -0.5, 1, // X Y Z	R G B A
+        1, 0, 1,    -0.5, -0.5, -0.5, 1, // X Y Z R G B A
+        1, 0, -1,    -0.5, -0.5, -0.5, 1, // X Y Z R G B A
+        -1, 0, -1,    -0.5, -0.5, -0.5, 1, // X Y Z R G B A
 
-		-1, 0, -1,	 -0.5, -0.5, -0.5, 1, // X Y Z	R G B A
-		-1, 0, 1,	 -0.5, -0.5, -0.5, 1, // X Y Z	R G B A
-		1, 0, 1,	 -0.5, -0.5, -0.5, 1, // X Y Z	R G B A
-	], 3 + 4)
+        -1, 0, -1,    -0.5, -0.5, -0.5, 1, // X Y Z R G B A
+        -1, 0, 1,    -0.5, -0.5, -0.5, 1, // X Y Z R G B A
+        1, 0, 1,    -0.5, -0.5, -0.5, 1, // X Y Z R G B A
+    ], 3 + 4);
 
-	let stride = (3 + 4) * Float32Array.BYTES_PER_ELEMENT
-	const basicShader = new Shader(gl, vertexShaderSource, fragmentShaderSource, {
-		vertexPosition: {name: "aVertexPosition", size: 3, stride: stride, offset: 0},
-		vertexColor: {name: "aVertexColor", size: 4, stride: stride, offset: 3 * Float32Array.BYTES_PER_ELEMENT},
-	}, {
-		fragmentColor: {name: "uFragColor", type: "Vector4"},
-		projectionMatrix: {name: "uProjectionMatrix", type: "Matrix4"},
-		modelViewMatrix: {name: "uModelViewMatrix", type: "Matrix4"},
-	})
+    let stride = (3 + 4) * Float32Array.BYTES_PER_ELEMENT;
+    const basicShader = new Shader(gl, vertexShaderSource, fragmentShaderSource, {
+        vertexPosition: {name: "aVertexPosition", size: 3, stride: stride, offset: 0},
+        vertexColor: {name: "aVertexColor", size: 4, stride: stride, offset: 3 * Float32Array.BYTES_PER_ELEMENT},
+    }, {
+        fragmentColor: {name: "uFragColor", type: "vec4"},
+        projectionMatrix: {name: "uProjectionMatrix", type: "mat4"},
+        modelViewMatrix: {name: "uModelViewMatrix", type: "mat4"},
+    });
 
-	let camera = new Camera(
-		new Vector3([0, 3, 10]), 
-		new Vector3([0, 0, 0]),
-		45,
-		gl.canvas.clientWidth / gl.canvas.clientHeight,
-		0.1,
-		1000.0,
-	);
+    let camera = new Camera(
+        vec3.fromValues(0, 3, 10),
+        vec3.fromValues(0, 0, 0),
+        45,
+        gl.canvas.clientWidth / gl.canvas.clientHeight,
+        0.1,
+        1000.0
+    );
 
-	let mainScene = new Scene(gl, camera, {
-		p1: new MeshInstance(pyramid, new ShaderInstance(basicShader), {fragmentColor: new Vector4([1, 0, 0, 1])}),
-		p2: new MeshInstance(pyramid, new ShaderInstance(basicShader), {fragmentColor: new Vector4([0, 0, 1, 1])}),
-	})
+    let mainScene = new Scene(gl, camera, {
+        p1: new MeshInstance(pyramid, new ShaderInstance(basicShader), {fragmentColor: vec4.fromValues(1, 0, 0, 1)}),
+        p2: new MeshInstance(pyramid, new ShaderInstance(basicShader), {fragmentColor: vec4.fromValues(0, 0, 1, 1)}),
+    });
 
-	mainScene.setup = (o) => {
-		o.p1.position = new Vector3([-2, 0, 0])
-		o.p2.position = new Vector3([2, 0, 0])
-	}
+    mainScene.setup = (o) => {
+        vec3.set(o.p1.position, -2, 0, 0);
+		vec3.set(o.p1.scale, 1, 2, 1);
+        vec3.set(o.p2.position, 2, 0, 0);
+    };
 
-	mainScene.update = (o, time, dt) => {
-		o.p1.rotation.rotate(1, 0.5, 1, -0.5)
-		o.p2.rotation.rotate(-1, -0.5, 1, 0.5)
+    mainScene.update = (o, time, dt) => {
+        mat4.rotate(o.p1.rotation, mat4.create(), (time * 3), [0.5, 1, -0.5]);
+        mat4.rotate(o.p2.rotation, mat4.create(), (time * -3), [-0.5, 1, 0.5]);
 
-		o.p1.position.elements[1] = Math.sin(time * 3)
-		o.p2.position.elements[1] = -Math.sin(time * 3)
+        vec3.set(o.p1.position, o.p1.position[0], Math.sin(time * 3), o.p1.position[2]);
+        vec3.set(o.p2.position, o.p2.position[0], -Math.sin(time * 3), o.p2.position[2]);
 
-		o.p1.shaderParams.fragmentColor.elements[1] = Math.sin(time * 4) * 2
-	}
+        vec4.set(o.p1.shaderParams.fragmentColor, 1, Math.sin(time * 4) * 2, 0, 1);
+    };
 
-	mainScene.start();
+    mainScene.start();
 }
-
-
-
